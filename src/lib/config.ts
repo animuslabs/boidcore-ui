fe753aa22c6478275e04ca8aea9b8e3ca04b0f2d
Seth@Boid.com <mchosc@animusystems.com>
2024-01-25 13:12:17 +0000

    fixed telos chain id

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -48,7 +48,7 @@ const configs:Record<Configs, Config> = {
       system: "boid"
     },
     linkData: {
-      chainId: "aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906",
+      chainId: "4667b205c6838ef70ff7988f6e8257e8be0e1284a2f59699054a018f743b1d11",
       nodeUrl: "https://mainnet.telos.net",
       appName: "boid-web"
     },
